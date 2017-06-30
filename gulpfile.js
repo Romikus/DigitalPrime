6d35f9562ff4feb87d28c293d1542549b12f2b42
Roman Naumov <romas.naumov@gmail.com>
2017-06-30 15:03:17 +0300

    Добавлена сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var browserSync = require("browser-sync").create();
 
+var rename = require("gulp-rename");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var run = require("run-sequence");
+
 gulp.task("style", function() {
     var prefixer = [autoprefixer({browsers: ["last 2 version"]})];
 
@@ -16,18 +22,46 @@ gulp.task("style", function() {
           includePaths: require('node-normalize-scss').includePaths
         }))
         .pipe(postcss(prefixer))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("build/css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task("svg", function() {
-    gulp.src("img/icon/*.svg")
-
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "images", fn);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html",
+    "*.png"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("serve", function() {
     browserSync.init({
-        server: ".",
+        server: "build",
         notify: false,
  		open: true,
 		cors: true,
@@ -36,4 +70,6 @@ gulp.task("serve", ["style"], function() {
 
     gulp.watch("sass/**/*{.scss,sass}", ["style"]);
     gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch("js/*.js").on("change", browserSync.reload);
 });
+
